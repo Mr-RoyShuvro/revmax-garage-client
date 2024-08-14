b312083fc837df13e698549a0ca6450e81c09ca9
Mr-RoyShuvro <shuvroroyshuvo@gmail.com>
2024-08-15 04:53:36 +0600

    login page design completed

---
+++ src/pages/auth/Login/Login.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import img from '../../../assets/images/login/login.svg'
+import { FcGoogle } from 'react-icons/fc';
+import { FaFacebookF, FaLinkedinIn } from 'react-icons/fa';
+
+
+const Login = () => {
+    return (
+        <div className="hero min-h-screen pt-14 pb-36">
+            <div className="hero-content flex-col lg:flex-row gap-20">
+                <div className="w-1/2 ">
+                    <img src={img} alt="" />
+                </div>
+                <div className="card w-1/2 shrink-0 px-20 border-2 border-[#D0D0D0]">
+                    <form className="w-[350px]">
+                        <h1 className="text-4xl font-semibold text-[#444] text-center pt-12 pb-10">Login</h1>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="text-lg font-semibold text-[#444] pb-5">Email</span>
+                            </label>
+                            <input type="email" placeholder="Your email" className="input input-bordered bg-white" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="text-lg font-semibold text-[#444] pt-7 pb-5">Confirm Password</span>
+                            </label>
+                            <input type="password" placeholder="Your password" className="input input-bordered bg-white" required />
+                        </div>
+                        <div className="form-control pt-7">
+                            <button className="btn bg-[#FF3811] text-base border-none text-white font-semibold hover:bg-[rgb(199,49,19)] hover:border-none">Sign In</button>
+                        </div>
+                        <h3 className='text-center text-[#444] text-lg font-medium py-6'>Or Sign In with</h3>
+                        <div className='flex gap-4 justify-center'>
+                            <div className='rounded-full bg-[#F5F5F8] p-5'>
+                                <FcGoogle className='text-2xl'></FcGoogle>
+                            </div>
+                            <div className='rounded-full bg-[#F5F5F8] p-5'>
+                                <FaFacebookF className='text-2xl text-[#3B5998]'></FaFacebookF>
+                            </div>
+                            <div className='rounded-full bg-[#F5F5F8] p-5'>
+                                <FaLinkedinIn className='text-2xl text-[#0A66C2]'></FaLinkedinIn>
+                            </div>
+                        </div>
+                        <h4 className='text-[#737373] text-lg font-normal text-center pt-8 pb-10'>Don't have an account? <span className='text-[#FF3811] font-bold'>Sign In</span></h4>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
